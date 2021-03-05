125e78efc007545c019f435cdf427095950deac2
박상우 <psw@bagsang-uui-MacBookPro.local>
2021-03-05 11:59:15 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://psw:psw0962%21@boilerplate.akg5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
